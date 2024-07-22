71c048b06970958f02927f9eeeddb07eb93afb59
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-07-22 10:24:54 +0530

    chore: Update ALLOWED_HOSTS for deployment and local development

--- bridges_data_consultancy/settings.py
+++ bridges_data_consultancy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.209.120.120', 'localhost', '127.0.0.1']
 
 
 # Application definition
